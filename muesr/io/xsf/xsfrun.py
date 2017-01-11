29e41b3cea136649a5a18b8d04ee8aa092580b4a
Pietro Bonfa <bonfus@gmail.com>
2017-01-11 08:16:58 +0100

    Don't crash if XCrysDen is not found

--- muesr/io/xsf/xsfrun.py
+++ muesr/io/xsf/xsfrun.py
@@ -1,7 +1,10 @@
 from muesr.settings import config
-import subprocess
+import subprocess, warnings
 
 def run_xcrysden(fname):
+    if config.XCrysExec == None:
+        warnings.warn("XCrysDen executable not found. Check configs.")
+        return
     p = subprocess.Popen([config.XCrysExec, "--xsf", fname], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     out, err = p.communicate()
 
