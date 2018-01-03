2a3c3be7610339ad047f4c537dcb0e6e4c4a4237
Pietro Bonfa <bonfus@gmail.com>
2018-01-03 21:33:11 +0100

    Single source file for docs and examples through jupyter notebooks

--- docs/conf.py
+++ docs/conf.py
@@ -35,6 +35,7 @@ extensions = [
     'sphinx.ext.coverage',
     'sphinx.ext.mathjax',
     'sphinx.ext.viewcode',
+    'nbsphinx'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -80,8 +81,7 @@ language = None
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = ['_build']
-
+exclude_patterns = ['_build', '**.ipynb_checkpoints']
 # The reST default role (used for this markup: `text`) to use for all
 # documents.
 #default_role = None
