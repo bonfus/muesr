89a6035c75255911f4a5edc674cb5cf4c17d2bf3
Pietro Bonfa <bonfus@gmail.com>
2018-02-15 21:57:09 +0100

    Missing origin shift in call to spacegroup_from_data

--- muesr/utilities/symsearch.py
+++ muesr/utilities/symsearch.py
@@ -31,20 +31,23 @@ def symsearch(sample, precision=1e-4):
     :rtype: bool
     """
     
-    if sample._check_lattice() and have_spg:
+    if sample._check_lattice():
+        # This may not identify rotation and translations correctly
+        #sample.sym = spg.get_spacegroup(sample.cell, symprec=precision)
+        
         dataset = spg.get_symmetry_dataset(sample.cell, symprec=precision)
         # spglib 1.9.9 returns a long int in python2, this causes problems
         # for spg.py, simple workaround is to convert it even if not necessary
         sample.sym = spacegroup_from_data(no=int(dataset['number']),
                                           rotations=dataset['rotations'],
-                                          translations=dataset['translations'])
+                                          translations=dataset['translations'],
+                                          subtrans=dataset['origin_shift'])
         
         warnings.warn("Warning, information regarding spacegroup setting might be wrong!",RuntimeWarning, stacklevel=0)
-        
-        return True
-    elif not have_spg:
-        warnings.warn("Warning, spglib not found. This function will always return False.")
-        return False
+        if sample.sym:
+            return True
+        else:
+            return False
     else:
         return False
      
