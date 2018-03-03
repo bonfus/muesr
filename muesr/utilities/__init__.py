24e73a3d3dc2e22d57b3086a369d58f890015d7e
Pietro Bonfa <bonfus@gmail.com>
2018-03-03 17:14:35 +0100

    Removed deprecated functions show_cell and show_supercell.

--- muesr/utilities/__init__.py
+++ muesr/utilities/__init__.py
@@ -1,6 +1,6 @@
 from .dft_grid import build_uniform_grid
 from .symsearch import symsearch
-from .xcrysden import show_structure
+from .visualize import show_structure
 from .ms import (mago_set_k, mago_add, mago_set_FC)
 from .muon import (muon_set_frac, muon_find_equiv, muon_reset)
 from .printer import print_cell
